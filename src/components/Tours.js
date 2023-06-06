f696e7e68aef5816b86fd1cfdf9d8e7fb5449690
Bobur112 <bahrommahmudov112@gmail.com>
2023-06-06 11:19:47 +0500

    size icon and fixed build command

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -6,7 +6,6 @@ const Tours = () => {
   return (
     <section className="section" id="tours">
       <Title title="featured " subTitle="tours" />
-
       <div className="section-center featured-center">
         {tours.map((tour) => {
           return <Tour key={tour.id} {...tour} />;
